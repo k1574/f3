c70f0b54b089d4ced88000f594c2a2dd7d342edd
k1574 <k1574@yandex.ru>
2022-02-07 20:03:38 +0500

    Use routines from objff.

--- ff-rand/main.c
+++ ff-rand/main.c
@@ -5,7 +5,7 @@
 #include <time.h>
 
 #include "../endian.h"
-#include "../farbfeld.h"
+#include <ff/ff.h>
 
 static char *argv0;
 
@@ -20,7 +20,7 @@ int
 main(int argc, char *argv[])
 {
 	uint32_t w, h, s, i;
-	Pixel p;
+	FFPixel p;
 	argv0 = argv[0] ;
 
 	if(argc!=3)
@@ -33,16 +33,9 @@ main(int argc, char *argv[])
 		usage();
 
 	s = w*h ;
-	if(islittleendian()){
-		swapendian(&h, 4);
-		swapendian(&w, 4);
-	}
-	
 	srand(time(0));
 
-	printf("farbfeld");
-	fwrite(&w, 4, 1, stdout);
-	fwrite(&h, 4, 1, stdout);
+	ff_write_header(1, w, h);
 
 	for( i=0 ; i<s ; ++i){
 		p.r = rand() % 2 ? rand() : rand() * 2 ;
